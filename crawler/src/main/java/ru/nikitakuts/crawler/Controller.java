d2f9f690558489e85f1323b1208b568559e701e1
ParzivalP1 <nikitakuts@mail.ru>
2023-02-24 13:02:39 +0300

    added RestTemplate

--- crawler/src/main/java/ru/nikitakuts/crawler/Controller.java
+++ crawler/src/main/java/ru/nikitakuts/crawler/Controller.java
@@ -2,12 +2,17 @@ package ru.nikitakuts.crawler;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("crawler")
 public class Controller {
     private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private RestTemplate template;
 
     @GetMapping("/hello")
     public String getBook() {
@@ -18,6 +23,7 @@ public class Controller {
     @GetMapping("/url")
     public String test(@RequestParam String address) {
         logger.info("test address {}", address);
-        return address;
+        var response = template.getForEntity(address, String.class);
+        return response.getBody();
     }
 }
--- crawler/src/main/java/ru/nikitakuts/crawler/Main.java
+++ crawler/src/main/java/ru/nikitakuts/crawler/Main.java
@@ -2,10 +2,20 @@ package ru.nikitakuts.crawler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 }
